1767952eb6cbee90f96a43fc2be29b96d64ea0be
m4rimolima <m4rimolima@gmail.com>
2025-11-21 19:56:06 -0300

    feat(migrations): criar tabelas de exposições, obras, artistas e logística

---
+++ app/Http/Controllers/ExposicaoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Exposicao;
+use Illuminate\Http\Request;
+
+class ExposicaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Exposicao $exposicao)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Exposicao $exposicao)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Exposicao $exposicao)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Exposicao $exposicao)
+    {
+        //
+    }
+}
