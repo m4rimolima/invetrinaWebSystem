3b60f845dd45f4d3a7ac2ad7f1f90f696ae60e95
m4rimolima <m4rimolima@gmail.com>
2025-11-21 20:41:24 -0300

    feat(routes): adicionar todas as rotas do sistema

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\ObraController;
@@ -8,17 +7,19 @@ use App\Http\Controllers\ExposicaoController;
 use App\Http\Controllers\LogisticaController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('artists', ArtistController::class);
+    Route::resource('obras', ObraController::class);
+    Route::resource('exposicoes', ExposicaoController::class);
+    Route::resource('logisticas', LogisticaController::class);
 });
 
 require __DIR__.'/auth.php';
