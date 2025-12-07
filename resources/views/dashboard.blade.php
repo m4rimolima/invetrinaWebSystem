c593fa1b5668a99cb43dde24a820b97f02974081
m4rimolima <m4rimolima@gmail.com>
2025-12-07 19:38:52 -0300

    feat: ajustando a mensagem com o nome de quem logou

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container mx-auto p-6 bg-white">
-    <h1 class="text-3xl font-bold mb-6 text-[#333D29] text-center">Olá, {{ auth()->user()->name }} seja bem vindo à galeria In Vetrina</h1>
+    <h1 class="text-xl font-semibold mb-6 text-[#333D29] text-left">Olá, {{ auth()->user()->name }} seja bem vindo à galeria In Vetrina</h1>
 
     <!-- Cards de Estatísticas -->
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
