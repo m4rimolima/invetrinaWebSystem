b1fd5f21137991ea4b7e8b2279f132a35532b0b0
m4rimolima <m4rimolima@gmail.com>
2025-12-07 20:30:22 -0300

    feat: ajustando lista duplicada no index DE LOGISTICAS

--- resources/views/logisticas/index.blade.php
+++ resources/views/logisticas/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+
 
 @section('content')
 
@@ -6,20 +6,19 @@
 
     <h1 class="text-2xl font-bold mb-4 text-[#333D29]">Logísticas</h1>
 
-    {{-- Sucesso --}}
+  
     @if(session('success'))
         <div class="bg-[#A4AC86] border border-[#656D4A] text-[#333D29] px-4 py-3 rounded-md mb-4">
             {{ session('success') }}
         </div>
     @endif
 
-    {{-- Botão adicionar --}}
+   
     <a href="{{ route('logisticas.create') }}" 
        class="bg-[#7F4F24] text-white px-4 py-2 rounded-md hover:bg-[#582F0E] transition mb-4 inline-block">
         Adicionar Logística
     </a>
 
-    {{-- TABELA --}}
     <div class="overflow-x-auto">
         <table class="w-full border border-[#A4AC86] rounded-md overflow-hidden">
 
@@ -47,16 +46,15 @@
                             {{ \Carbon\Carbon::parse($logistica->data_transporte)->format('d/m/Y') }}
                         </td>
 
-                        {{-- AÇÕES --}}
+                      
                         <td class="py-2 px-4 border-b border-[#A4AC86] flex items-center gap-2">
 
-                            {{-- Editar --}}
+                           
                             <a href="{{ route('logisticas.edit', $logistica->id) }}"
                                class="px-3 py-1 rounded-md bg-[#A4AC86] text-[#333D29] hover:bg-[#7F4F24] hover:text-white transition text-sm">
                                Editar
                             </a>
 
-                            {{-- Excluir --}}
                             <form action="{{ route('logisticas.destroy', $logistica->id) }}"
                                   method="POST"
                                   onsubmit="return confirm('Tem certeza que deseja excluir?');">
